474521bd101c9dc37375629067837054e683286e
Philippe Turcotte <turcotte87@hotmail.com>
2026-06-10 14:56:57 -0400

    modification test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          whut
+          Bonjour
         </p>
         <a
           className="App-link"
