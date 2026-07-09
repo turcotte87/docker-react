510b15bff19f01370e3bca01f4b41c9e0ed4567c
Philippe Turcotte <turcotte87@hotmail.com>
2026-07-09 10:03:23 -0400

    ajout aws key

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bonjour
+          Bonjour, comment ca va
         </p>
         <a
           className="App-link"
